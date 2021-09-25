cf42b35e0b1e65fd5002340de09cc52707462027
lbeltranbr <57176574+lbeltranbr@users.noreply.github.com>
2021-09-25 11:19:32 +0100

    update

---
+++ Assets/Scripts/Create_Maze.cs
@@ -0,0 +1,308 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Create_Maze : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float[,] maze;
+    //private float[,] walls_pos;
+
+    public GameObject wall;
+    public Button generate;
+    public GameObject object_maze;
+
+    struct Cell
+    {
+        Vector2 position;
+       
+        public Cell(float x, float y)
+        {
+            this.position.x = x;
+            this.position.y = y;
+          
+        }
+        public Vector2 GetPos()
+        {
+            return position;
+        }
+    }
+
+    private Cell[,] walls_pos;
+
+    // List to hold unvisited cells.
+    private List<Cell> unvisited = new List<Cell>();
+    // List to store 'stack' cells, cells being checked during generation.
+    private List<Cell> stack = new List<Cell>();
+
+    private Cell current;
+    private Cell check;
+    private Vector2[] neighbourPositions = new Vector2[] { new Vector2(-2, 0), new Vector2(2, 0), new Vector2(0, 2), new Vector2(0, -2) };
+
+    void Start()
+    {
+        maze = new float[13, 12] {
+            { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 3, -1 },
+            { -1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, -1 },
+            { -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, -1 },
+            { -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1 },
+            { -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1 },
+            { -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1 },
+            { -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1 },
+            { -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1 },
+            { -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1 },
+            { -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1 },
+            { -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1 },
+            { -1, 2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
+            { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}
+        }; 
+        walls_pos = new Cell[13, 12];
+        InitWallsMatrix();
+        GenerateMaze();
+
+    }
+
+    public void GenerateMaze()
+    {
+        DestroyMaze();
+        InitUnvisited();
+
+        Vector2 startPos = new Vector2(10, 1);
+
+        current = unvisited[FindByPosition(startPos)];
+
+        unvisited.RemoveAt(FindByPosition(startPos));
+
+        bool end_pos = false;
+
+        while (unvisited.Count() > 0)
+        {
+            maze[(int)current.GetPos().x, (int)current.GetPos().y] = 0;
+
+            //check unvisited neighbours
+            List<Cell> not_visited_neighbours = new List<Cell>();
+
+            Vector2 F = current.GetPos() + neighbourPositions[0];
+            Vector2 B = current.GetPos() + neighbourPositions[1];
+            Vector2 R = current.GetPos() + neighbourPositions[2];
+            Vector2 L = current.GetPos() + neighbourPositions[3];
+
+
+            if (IsInUnvisited(L))
+                not_visited_neighbours.Add(unvisited[FindByPosition(L)]);
+            if (IsInUnvisited(R))
+                not_visited_neighbours.Add(unvisited[FindByPosition(R)]);
+            if (IsInUnvisited(B))
+                not_visited_neighbours.Add(unvisited[FindByPosition(B)]);
+            if (IsInUnvisited(F))
+                not_visited_neighbours.Add(unvisited[FindByPosition(F)]);
+
+
+            if (not_visited_neighbours.Count() > 0)
+            {
+                float rnd = UnityEngine.Random.Range(0.0f, 1.0f);
+
+                if (not_visited_neighbours.Count() == 1)
+                    check = not_visited_neighbours[0];
+
+                if (not_visited_neighbours.Count() == 2)
+                {
+                    
+                    if (rnd < 0.5)
+                        check = not_visited_neighbours[0];
+                    else
+                        check = not_visited_neighbours[1];
+
+                }
+                if (not_visited_neighbours.Count() == 3)
+                {
+                    if (rnd < 0.33)
+                        check = not_visited_neighbours[0];
+                    else if (rnd > 0.33 && rnd < 0.66)
+                        check = not_visited_neighbours[1];
+                    else
+                        check = not_visited_neighbours[2];
+                }
+                if (not_visited_neighbours.Count() == 4)
+                {
+                    if (rnd < 0.25)
+                        check = not_visited_neighbours[0];
+                    else if (rnd > 0.25 && rnd < 0.5)
+                        check = not_visited_neighbours[1];
+                    else if (rnd > 0.5 && rnd < 0.75)
+                        check = not_visited_neighbours[2];
+                    else
+                        check = not_visited_neighbours[3];
+
+                }
+
+                for (int i = 0; i < not_visited_neighbours.Count(); i++)
+                {
+                    if (not_visited_neighbours[i].GetPos() == new Vector2(2, 9))
+                    {
+                        check = not_visited_neighbours[i];
+
+                    }
+                }
+
+                stack.Add(current);
+                RemoveWalls();
+                current = check;
+                unvisited.RemoveAt(FindByPosition(current.GetPos()));
+
+            }
+            else if (stack.Count() > 0)
+            {
+                current = stack[stack.Count() - 1];
+                stack.Remove(current);
+            }
+            PrintMaze();
+        }
+        CreateWalls();
+
+    }
+    private void DestroyMaze()
+    {
+        foreach (Transform child in object_maze.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+
+        maze = new float[13, 12] {
+            { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 3, -1 },
+            { -1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, -1 },
+            { -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, -1 },
+            { -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1 },
+            { -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1 },
+            { -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1 },
+            { -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1 },
+            { -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1 },
+            { -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1 },
+            { -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1 },
+            { -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1 },
+            { -1, 2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
+            { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}
+        };
+    }
+    private void CreateWalls()
+    {
+        for (int i = 1; i < 12; i ++)
+        {
+            for (int j = 1; j < 11; j ++)
+            {
+                if (maze[i, j] == 1)
+                {
+                    float x = walls_pos[i, j].GetPos().x;
+                    float z = walls_pos[i, j].GetPos().y;
+                    Vector3 coord = new Vector3(x, 0.5f, z);
+                    var newObj= Instantiate(wall, coord, object_maze.transform.rotation);
+                    newObj.transform.parent = object_maze.transform;
+                }
+            }
+        }
+    }
+    private void InitWallsMatrix()
+    {
+        float x = -4.5f;
+        float z = 4.5f;
+
+        for(int i = 0; i < 13; i++)
+        {
+            
+            for (int j = 0; j < 12; j++)
+            {
+                if (i == 0 || i == 11 || i == 12||j==0||j==11)
+                {
+                    Cell cell = new Cell(0, 0);
+                    walls_pos[i, j] = cell;
+                }
+                else
+                {
+                    Cell cell = new Cell(x, z);
+                    walls_pos[i, j] = cell;
+                    x ++;
+                }
+
+
+            }
+
+            if (i != 0 && i != 11 && i != 12)
+            {
+                z--;
+                x = -4.5f;
+            }
+        }
+    }
+    private void InitUnvisited()
+    {
+
+        for (int i = 2; i < 12; i += 2)
+        {
+            for (int j = 1; j < 11; j += 2)
+            {
+                Cell cell = new Cell(i, j);
+                unvisited.Add(cell);
+
+            }
+        }
+    }
+    private int FindByPosition(Vector2 pos)
+    {
+        int position = 0;
+
+        foreach(var it in unvisited)
+        {
+            if (it.GetPos() == pos)
+            {
+                position = unvisited.IndexOf(it);
+            }
+        }
+
+        return position;
+    }
+    private bool IsInUnvisited(Vector2 pos)
+    {
+        bool position = false;
+
+        foreach (var it in unvisited)
+        {
+            if (it.GetPos() == pos)
+            {
+                position = true;
+            }
+        }
+
+        return position;
+    }
+    private void RemoveWalls()
+    {
+        if(check.GetPos().x < current.GetPos().x) //forward
+            maze[(int)check.GetPos().x+1,(int) check.GetPos().y] = 0;
+        if (check.GetPos().x > current.GetPos().x) //backward
+            maze[(int)check.GetPos().x - 1, (int)check.GetPos().y] = 0;
+
+        if (check.GetPos().y < current.GetPos().y) //left
+            maze[(int)check.GetPos().x, (int)check.GetPos().y+1] = 0;
+        if (check.GetPos().y > current.GetPos().y) //right
+            maze[(int)check.GetPos().x, (int)check.GetPos().y-1] = 0;
+
+    }
+    private void PrintMaze()
+    {
+        int rowLength = maze.GetLength(0);
+        int colLength = maze.GetLength(1);
+        string arrayString = "";
+        for (int i = 0; i < rowLength; i++)
+        {
+            for (int j = 0; j < colLength; j++)
+            {
+                arrayString += string.Format("{0} ", maze[i, j]);
+            }
+            arrayString += System.Environment.NewLine + System.Environment.NewLine;
+        }
+
+        //Debug.Log(arrayString);
+    }
+}
---
+++ Assets/Scripts/Move_Player.cs
@@ -0,0 +1,340 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using System.Threading;
+
+public class Move_Player : MonoBehaviour
+{
+
+    private Vector3 init_pos;
+    private Quaternion init_rot;
+    private Rigidbody m_Rigidbody;
+    private float[,] maze;
+    private bool goal;
+    private float[,] Q;
+    private int state;
+    private int next_state;
+    private int action;
+    private int reward;
+
+    public Button learn;
+    public bool debug_move;
+    public float vel;
+    public float vel_rot;
+    public float gamma;
+    public float alpha;
+    public int episodes;
+    public int max_moves;
+    public float epsilon;
+    public Text episode_text;
+
+    enum actions{FORWARD, BACKWARD, LEFT ,RIGHT }; //0,1,2,3
+    enum states{START, CORRECT_PATH, HIT, END };//0,1,2,3.4
+    // Start is called before the first frame update
+    struct move
+    {
+        Vector3 position;
+        int ep;
+
+        public move(float x, float y,float z,int e)
+        {
+            this.position.x = x;
+            this.position.y = y;
+            this.position.z = z;
+            this.ep = e;
+
+        }
+        public Vector3 GetPos()
+        {
+            return position;
+        }
+        public int GetEpisode()
+        {
+            return ep;
+        }
+    }
+    private List<move> moves_list =new List<move>();
+    void Start()
+    {
+        init_pos = this.gameObject.transform.position;
+        init_rot = this.gameObject.transform.rotation;
+        goal = true;
+
+        //initialize Q with 0
+        Q = new float[4, 4];
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+            Reset();
+
+
+        if (debug_move)
+        {
+            float velocity=vel* Time.deltaTime;
+            float velocity_rot=vel_rot* Time.deltaTime*15;
+
+            if (Input.GetKey(KeyCode.W))
+                this.transform.position = this.transform.position + transform.forward * velocity;
+
+            if (Input.GetKey(KeyCode.S))
+                this.transform.position = this.transform.position - transform.forward * velocity;
+           
+            if (Input.GetKey(KeyCode.D))
+                this.transform.Rotate(new Vector3(0, 1, 0) * velocity_rot, Space.World);
+
+            if (Input.GetKey(KeyCode.A))
+                this.transform.Rotate(new Vector3(0, -1, 0) * velocity_rot, Space.World);
+        }
+       
+    }
+
+    //********************************* REINFORCEMENT LEARNING *********************************//
+
+    public void RunLearning()
+    {
+        // clear the list of movements
+        moves_list.Clear();
+        Q = new float[4, 4];
+
+
+        // get the maze matrix
+        maze = this.GetComponent<Create_Maze>().maze;
+
+        //add first position to the list of movements
+        move m = new move(init_pos.x, init_pos.y, init_pos.z, 0);
+        moves_list.Add(m);
+
+        // start learning
+        Reinforcement_learning();
+        
+        // show movements
+        StartCoroutine(MovePlayer());
+        
+    }
+    private void Reinforcement_learning()
+    {
+        
+        //for each episode 
+        for(int i=0; i < episodes; i++)
+        {
+            state = (int)states.START;
+            Vector2 init_coord = new Vector2(11, 1);
+            Vector2 current_coord = init_coord;
+            Vector2 state_coord;
+            Vector3 position; 
+            int moves = 0;
+            goal = true;
+            //Debug.Log("Episode: " + i);
+
+            //do while it doesn't reach the goal
+            while (goal)
+            {
+                Debug.Log("State: " + state);
+
+                //select one action
+                action = ChooseAction();
+                state_coord = current_coord;
+                position = this.transform.position;
+                //move according to action and update coordinates
+                switch (action) 
+                {
+                    case (int)actions.FORWARD:
+                        Debug.Log("Forward");
+                        current_coord += new Vector2(-1, 0);
+                        Forward();
+                        break;
+                    case (int)actions.BACKWARD:
+                        Debug.Log("Backward");
+
+                        current_coord += new Vector2(1, 0);
+                        Backward();
+                        break;
+                    case (int)actions.LEFT:
+                        Debug.Log("Left");
+
+                        current_coord += new Vector2(0, -1);
+                        TurnLeft();
+                        break;
+                    case (int)actions.RIGHT:
+                        Debug.Log("Right");
+
+                        current_coord += new Vector2(0, 1);
+                        TurnRight();
+                        break;
+
+                }
+                //Thread.Sleep(5000);
+                //Debug.Log("position: " +this.transform.position);
+                moves++;
+
+                reward = GetReward(current_coord);
+                //Debug.Log("Reward: " + reward);
+
+                if (reward == -6)
+                {
+                    current_coord = state_coord;
+
+                    this.transform.position = position;
+                }
+
+                move m = new move(this.transform.position.x, this.transform.position.y, this.transform.position.z, i);
+                moves_list.Add(m);
+
+                Q[state, action] += alpha * (reward+gamma*GetMaxQ()- Q[state, action]);
+                Debug.Log(Q);
+
+                state = next_state;
+
+                if (state == (int)states.END || moves == max_moves)
+                {
+                    Reset();
+                    goal = false;
+
+                }
+
+            }
+        }
+
+
+    }
+    private int ChooseAction()
+    {
+
+        float rnd = UnityEngine.Random.Range(0.0f, 1.0f);
+        int action_coord = GetMaxQCoord();
+        float a = 0;
+        if (rnd > epsilon)
+            a = action_coord;
+        else
+            a = (float)Math.Floor(UnityEngine.Random.Range(0.0f, 3.5f));
+
+        return (int)a;
+
+    }
+    private int GetReward( Vector2 coord)
+    {
+        
+            //If the goal is reached
+            if (maze[(int)coord.x, (int)coord.y] == 3)
+            {
+                next_state = (int)states.END;
+                return 10;
+
+            }
+            //If start
+            else if(maze[(int)coord.x, (int)coord.y] == 2)
+            {
+                next_state = (int)states.START;
+                return -10;
+            }
+            //if correct path
+            else if (maze[(int)coord.x, (int)coord.y] == 0)
+            {
+                next_state = (int)states.CORRECT_PATH;
+                return 1;
+            }
+            //if hit
+            else
+            {
+                next_state = (int)states.HIT;
+                return -6;
+
+            }
+        
+    }
+    private float GetMaxQ()
+    {
+        //get max value of Q
+        float max= Q[next_state, 0];
+
+        for(int i = 0; i < 4; i++)
+        {
+            float value = Q[next_state, i];
+            if (value > max)
+                max = value;
+        }
+
+        return max;
+    }
+    private int GetMaxQCoord()
+    {
+        // Get the coordinates of the max value of Q
+        float max = Q[next_state, 0];
+        int temp = 0;
+
+        for (int i = 0; i < 4; i++)
+        {
+            float value = Q[next_state, i];
+            if (value > max)
+            {
+                max = value;
+                temp = i;
+            }
+        }
+
+        return temp;
+    }
+
+    //********************************* END REINFORCEMENT LEARNING *********************************//
+
+
+    //********************************* RESET POSITION *********************************//
+
+    private void Reset()
+    {
+        this.transform.position = init_pos;
+        this.transform.rotation = init_rot;
+
+    }
+    //********************************* END RESET POSITION *********************************//
+
+
+    //********************************* MOVEMENT FUNCTIONS *********************************//
+    private void TurnLeft()
+    {
+        this.transform.Translate(new Vector3(-1, 0, 0));
+      
+        
+    }
+    private void TurnRight()
+    {
+        this.transform.Translate(new Vector3(1, 0, 0));
+        
+    }
+    private void Forward()
+    {
+        this.transform.Translate(new Vector3(0, 0, 1));
+    
+
+    }
+    private void Backward()
+    {
+        this.transform.Translate(new Vector3(0, 0, -1));
+        
+
+    }
+
+    private IEnumerator MovePlayer()
+    {
+        int count = 0;
+        foreach (var it in moves_list)
+        {
+            count++;
+            this.transform.SetPositionAndRotation(it.GetPos(), init_rot);
+            episode_text.text = "Episode: " + (it.GetEpisode() + 1).ToString() + "\n" + "Move: " + count;
+            yield return new WaitForSeconds(0.05f);
+            if(count==max_moves)
+                count = 0;
+        }
+        Reset();
+    }
+    //********************************* END MOVEMENT FUNCTIONS *********************************//
+
+}
